e4d815be354b4f0d6174401511fff23543aa98fb
2055619 <2055619@claurendeau.qc.ca>
2024-03-02 14:52:27 -0500

    Nouveau Nom

--- cookbook/src/main/java/quixotic/projects/cookbook/exception/badRequestException/UsernameTakenException.java
+++ cookbook/src/main/java/quixotic/projects/cookbook/exception/badRequestException/UsernameTakenException.java
@@ -2,6 +2,6 @@ package quixotic.projects.cookbook.exception.badRequestException;
 
 public class UsernameTakenException extends BadRequestException {
     public UsernameTakenException() {
-        super("messages.usernameTaken");
+        super("message.usernameTaken");
     }
 }
