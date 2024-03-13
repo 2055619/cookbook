5c44d116faeec507b4be64dd7b2ecaa834d2d260
2055619 <2055619@claurendeau.qc.ca>
2024-03-13 16:45:26 -0400

    Concoction Start

--- cookbook/cookbook/src/pages/user/ConcoctRecipe.tsx
+++ cookbook/cookbook/src/pages/user/ConcoctRecipe.tsx
@@ -2,7 +2,7 @@ import {useEffect, useState} from "react";
 import {toast} from "react-toastify";
 import {CookBookService} from "../../services/CookBookService";
 import {useTranslation} from "react-i18next";
-import {IRecipe} from "../../assets/models/Recipe";
+import {IIngredient, IRecipe} from "../../assets/models/Recipe";
 
 function ConcoctRecipe() {
     const cookbookService = new CookBookService();
@@ -25,6 +25,9 @@ function ConcoctRecipe() {
         }
     );
 
+    const [ingredientUnits, setIngredientUnits] = useState<{[key: string]: string}>({});
+    const [checkedIngredients, setCheckedIngredients] = useState<string[]>([]);
+
     useEffect(() => {
         const urlParams = new URLSearchParams(window.location.search);
         const title = urlParams.get('title');
@@ -44,12 +47,54 @@ function ConcoctRecipe() {
         }
     }, []);
 
+    const handleUnitChange = (ingredient: IIngredient, unit: string) => {
+        // Here you can add the logic to calculate the equivalent amount in the selected unit
+        // For simplicity, let's assume that the equivalent amount is the same as the original amount
+        const equivalentAmount = ingredient.quantity;
+
+        setIngredientUnits(prevState => ({
+            ...prevState,
+            [ingredient.name]: `${equivalentAmount} ${unit}`
+        }));
+    };
+
+    const handleCheck = (ingredient: IIngredient) => {
+        if (checkedIngredients.includes(ingredient.name)) {
+            setCheckedIngredients(checkedIngredients.filter(i => i !== ingredient.name));
+        } else {
+            setCheckedIngredients([...checkedIngredients, ingredient.name]);
+        }
+    };
 
     return (
-    <div>
-      <h1>Concoct Recipe</h1>
-    </div>
-  );
+        <div>
+            <h1 className={"text-9xl"}>{recipe.title}</h1>
+
+            <ul>
+                {recipe.ingredients.filter(ingredient => !checkedIngredients.includes(ingredient.name)).map((ingredient, index) => (
+                    <li key={index}>
+                        <input type="checkbox" id={`ingredient-${index}`} onChange={() => handleCheck(ingredient)}/>
+                        <label htmlFor={`ingredient-${index}`}>{ingredient.name}</label>
+                        {/* Rest of the ingredient item */}
+                    </li>
+                ))}
+            </ul>
+            <div className="mt-4">
+                <h2>Checked Ingredients</h2>
+                <ul>
+                    {recipe.ingredients.filter(ingredient => checkedIngredients.includes(ingredient.name)).map((ingredient, index) => (
+                        <li key={index} className="text-gray-500">
+                            <input type="checkbox" id={`ingredient-${index}`} checked
+                                   onChange={() => handleCheck(ingredient)}/>
+                            <label htmlFor={`ingredient-${index}`}>{ingredient.name}</label>
+                            {/* Rest of the ingredient item */}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+
+        </div>
+    );
 }
 
 export default ConcoctRecipe;
