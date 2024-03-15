f6224013e4b8733c6c271d22c144250aacd16258
2055619 <2055619@claurendeau.qc.ca>
2024-03-14 21:00:57 -0400

    Other info

--- cookbook/cookbook/src/pages/user/ConcoctRecipe.tsx
+++ cookbook/cookbook/src/pages/user/ConcoctRecipe.tsx
@@ -199,6 +199,19 @@ function ConcoctRecipe() {
 
             <InstructionCard recipe={recipe}/>
 
+            <div>
+                <h1 className={"text-5xl my-3"}>{t('otherInfo')}</h1>
+                <h3 className={"text-2xl mb-2"}>{recipe.description}</h3>
+                <div className={"grid grid-cols-5 gap-y-3"}>
+                    <p>{t('category') + ": " + t(recipe.category)}</p>
+                    <p>{t('difficulty') + ": " + t(recipe.difficulty)}</p>
+                    <p>{t('portionSize') + ": " + t(recipe.portionSize)}</p>
+                    <p>{t('serving') + ": " + recipe.serving}</p>
+                    <p>{t('visibility') + ": " + t(recipe.visibility)}</p>
+                    <p className={"col-span-5"}>{t('dietTypes') + ": " + recipe.dietTypes.map(dietType => t(dietType)).join(", ")}</p>
+                </div>
+
+            </div>
 
         </div>
     );
